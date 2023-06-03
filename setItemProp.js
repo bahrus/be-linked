55ffb5a82ad7da2a07bcdf2b1678799a96f03844
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-06-03 08:28:39 -0400

    0.0.12

--- setItemProp.js
+++ setItemProp.js
@@ -1,32 +1,21 @@
 export async function setItemProp(el, val) {
+    let intl;
     switch (el.localName) {
         case 'data':
         case 'output':
         case 'time':
             import('be-intl/be-intl.js');
-            await el.beEnhanced.whenResolved('be-intl');
-            break;
+            intl = await el.beEnhanced.whenResolved('be-intl');
+            intl.value = val;
+            return;
         case 'link':
             import('be-link-valued/be-link-valued.js');
             await el.beEnhanced.whenResolved('be-link-valued');
     }
-    switch (el.localName) {
-        case 'data':
-        case 'output':
-            el.value = val;
-            break;
-        case 'time':
-            el.dateTime = val;
-            break;
-        case 'link':
-            el.beEnhanced.beLinkValued.value = val;
-            break;
-        default:
-            if (el.href !== undefined) {
-                el.href = val;
-            }
-            else {
-                el.textContent = val; //TODO, many more cases to consider
-            }
+    if (el.href !== undefined) {
+        el.href = val;
+    }
+    else {
+        el.textContent = val; //TODO, many more cases to consider
     }
 }
--- setItemProp.ts
+++ setItemProp.ts
@@ -1,33 +1,23 @@
+import {AllProps} from 'be-intl/types';
 export async function setItemProp(el: Element, val: any){
+    let intl: AllProps;
     switch(el.localName){
         case 'data':
         case 'output':
         case 'time':
             import('be-intl/be-intl.js');
-            await (<any>el).beEnhanced.whenResolved('be-intl');
-            break;
+            intl = await (<any>el).beEnhanced.whenResolved('be-intl');
+            intl.value = val;
+            return;
         case 'link':
             import('be-link-valued/be-link-valued.js');
             await (<any>el).beEnhanced.whenResolved('be-link-valued');
     }
-    switch(el.localName){
-        case 'data':
-        case 'output':
-            (<HTMLDataElement>el).value = val;
-            break;
-        case 'time':
-            (<HTMLTimeElement>el).dateTime = val;
-            break;
-        case 'link':
-            (<any>el).beEnhanced.beLinkValued.value = val;
-            break;
-        default:
-            if((<any>el).href !== undefined){
-                (<any>el).href = val;
-            }else{
-                el.textContent = val;//TODO, many more cases to consider
-            }
-            
+    if((<any>el).href !== undefined){
+        (<any>el).href = val;
+    }else{
+        el.textContent = val;//TODO, many more cases to consider
     }
+
     
 }
