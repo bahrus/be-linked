2856394f9540312291fd64f86f3ed77eb427799d
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-06-04 12:26:16 -0400

    0.0.15

---
+++ getItemScopeObject.js
@@ -0,0 +1,12 @@
+export async function getItemScopeObject(el) {
+    if (!el.hasAttribute('itemscope'))
+        throw { el, msg: 'itemscope missing.' };
+    const derivedObject = {};
+    //TODO:  use @scope selector
+    let itempropElements = Array.from(el.querySelectorAll('[itemprop]'));
+    itempropElements = itempropElements.filter(x => x.closest('[itemscope]') === el);
+    const { getItemPropVal } = await import('./getItemPropVal.js');
+    for (const itempropElement of itempropElements) {
+        derivedObject[itempropElement.getAttribute('itemprop')] = await getItemPropVal(itempropElement);
+    }
+}
---
+++ getItemScopeObject.ts
@@ -0,0 +1,13 @@
+import { JSONObject } from 'trans-render/lib/types';
+
+export async function getItemScopeObject(el: Element){
+    if(!el.hasAttribute('itemscope')) throw {el, msg: 'itemscope missing.'};
+    const derivedObject: JSONObject = {};
+    //TODO:  use @scope selector
+    let itempropElements = Array.from(el.querySelectorAll('[itemprop]'));
+    itempropElements = itempropElements.filter(x => x.closest('[itemscope]') === el);
+    const {getItemPropVal} = await import('./getItemPropVal.js');
+    for(const itempropElement of itempropElements){
+        derivedObject[itempropElement.getAttribute('itemprop')!] = await getItemPropVal(itempropElement);
+    }
+}
