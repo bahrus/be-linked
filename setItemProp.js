b981cb178930689a6282d1a4a4c3308b9d34fae1
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-07-09 16:31:38 -0400

    0.0.34

--- setItemProp.js
+++ setItemProp.js
@@ -41,58 +41,31 @@ export async function setItemProp(el, val, name) {
             const aSrc = el;
             if (el.hasAttribute('itemscope')) {
                 if (Array.isArray(val)) {
+                    //loop scenario
                     import('be-repeated/be-repeated.js');
                     const beRepeated = await aSrc.beEnhanced.whenResolved('be-repeated');
                     beRepeated.rowHandler = async (row) => {
                         await handleRow(row, val);
                     };
-                    // beRepeated.addEventListener('rows', async (e: Event) => {
-                    //     console.log('start rows');
-                    //     const rows = (e as CustomEvent).detail.rows as Row[];
-                    //     for(const row of rows){
-                    //         const {idx, children, condition} = row;
-                    //         //TODO:  support defer rendering based on condition === existing
-                    //         const item = val[idx - 1];
-                    //         for(const child of children){
-                    //             const itemProp = child.getAttribute('itemprop');
-                    //             if(itemProp === 'itemListElement'){
-                    //                 await setItemProp(child, item, itemProp);
-                    //             }
-                    //         }
-                    //     }
-                    //     console.log('finished rows');
-                    // });
                     Object.assign(beRepeated, {
                         startIdx: 1,
                         endIdx: val.length,
                         templIdx: 0
                     });
-                    //loop scenario
                 }
                 else {
-                    //if(el.hasAttribute('itemscope')){
                     if (val.constructor.toString().startsWith('class ')) {
+                        //use propagator
                         import('be-propagating/be-propagating.js');
                         const bePropagating = await aSrc.beEnhanced.whenResolved('be-propagating');
                         bePropagating.setKeyVal(name, val);
-                        //use propagator
                     }
                     else {
                         //assign into scope
                         import('be-scoped/be-scoped.js');
-                        const beSpoked = await aSrc.beEnhanced.whenResolved('be-scoped');
-                        // for(const key in val){
-                        //     const keyVal = val[key];
-                        //     if(typeof keyVal === 'object'){
-                        //         //TODO
-                        //     }else{
-                        //         beSpoked.setKeyVal(key, keyVal);
-                        //     }
-                        // }
-                        beSpoked.setKeyVal(name, val);
+                        const beScoped = await aSrc.beEnhanced.whenResolved('be-scoped');
+                        beScoped.setKeyVal(name, val);
                     }
-                    // }else{
-                    // }
                 }
             }
             else {
--- setItemProp.ts
+++ setItemProp.ts
@@ -44,62 +44,30 @@ export async function setItemProp(el: Element, val: any, name: string){
             const aSrc = el as any;
             if(el.hasAttribute('itemscope')){
                 if(Array.isArray(val)){
+                    //loop scenario
                     import('be-repeated/be-repeated.js');
                     const beRepeated = await aSrc.beEnhanced.whenResolved('be-repeated') as BeRepeatedAllProps;
                     beRepeated.rowHandler = async (row: Row) => {
                         await handleRow(row, val);
                     }
-                    // beRepeated.addEventListener('rows', async (e: Event) => {
-                    //     console.log('start rows');
-                    //     const rows = (e as CustomEvent).detail.rows as Row[];
-                    //     for(const row of rows){
-                    //         const {idx, children, condition} = row;
-                    //         //TODO:  support defer rendering based on condition === existing
-                    //         const item = val[idx - 1];
-                    //         for(const child of children){
-                    //             const itemProp = child.getAttribute('itemprop');
-                    //             if(itemProp === 'itemListElement'){
-                    //                 await setItemProp(child, item, itemProp);
-                    //             }
-                    //         }
-    
-                    //     }
-                    //     console.log('finished rows');
-                    // });
-                    
                     Object.assign(beRepeated, {
                         startIdx: 1,
                         endIdx: val.length,
                         templIdx: 0
                     } as BeRepeatedEndUserProps);
-                    //loop scenario
                 }else{
                     
-                    //if(el.hasAttribute('itemscope')){
-                        if(val.constructor.toString().startsWith('class ')){
-                            import('be-propagating/be-propagating.js');
-                            const bePropagating = await aSrc.beEnhanced.whenResolved('be-propagating') as bePropagatingActions;
-                            bePropagating.setKeyVal(name, val);
-                            //use propagator
-                        }else{
-                            //assign into scope
-                            import('be-scoped/be-scoped.js');
-                            const beSpoked = await aSrc.beEnhanced.whenResolved('be-scoped') as beScopedActions;
-                            // for(const key in val){
-                            //     const keyVal = val[key];
-                            //     if(typeof keyVal === 'object'){
-                            //         //TODO
-                            //     }else{
-                            //         beSpoked.setKeyVal(key, keyVal);
-                            //     }
-                                
-                            // }
-                            beSpoked.setKeyVal(name, val);
-                        }
-                    // }else{
-                        
-                    // }
-    
+                    if(val.constructor.toString().startsWith('class ')){
+                        //use propagator
+                        import('be-propagating/be-propagating.js');
+                        const bePropagating = await aSrc.beEnhanced.whenResolved('be-propagating') as bePropagatingActions;
+                        bePropagating.setKeyVal(name, val);
+                    }else{
+                        //assign into scope
+                        import('be-scoped/be-scoped.js');
+                        const beScoped = await aSrc.beEnhanced.whenResolved('be-scoped') as beScopedActions;
+                        beScoped.setKeyVal(name, val);
+                    }
                 }
             }else{
                 el.textContent = toString(val, 40);// JSON.stringify(val, null, 2);
