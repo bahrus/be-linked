e28142b56e4ff955c5b094baa3e73d5f0ed72c04
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-07-16 10:23:10 -0400

    0.0.39

--- applyEnh.ts
+++ applyEnh.ts
@@ -1,5 +1,6 @@
+import {camelToLisp} from 'trans-render/lib/camelToLisp.js';
 export async function applyEnh(instance: Element, enhancement: string, wait?: boolean){
-    const {camelToLisp} = await import('trans-render/lib/camelToLisp.js');
+    //const {camelToLisp} = await import('trans-render/lib/camelToLisp.js');
     const enh = camelToLisp(enhancement);
     if(wait){
         return await (<any>instance).beEnhanced.whenResolved(enh);
