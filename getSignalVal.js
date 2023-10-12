7b639e09fc7ca4997ac4dcc832e196f9592ce758
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2023-10-12 17:44:38 -0400

    0.0.53

--- getSignalVal.js
+++ getSignalVal.js
@@ -8,6 +8,16 @@ export function getSignalVal(obj) {
         if (obj.hasAttribute('aria-checked')) {
             return obj.getAttribute('aria-checked') === 'true';
         }
+        if ('valueAsNumber' in obj) {
+            if (obj instanceof HTMLInputElement && obj.type === 'number') {
+                return obj.valueAsNumber;
+            }
+        }
+        if ('valueAsDate' in obj) {
+            if (obj instanceof HTMLInputElement && obj.type === 'date') {
+                return obj.valueAsDate;
+            }
+        }
         if ('value' in obj) {
             return obj.value;
         }
--- getSignalVal.ts
+++ getSignalVal.ts
@@ -10,6 +10,16 @@ export function getSignalVal(obj: SignalRefType){
         if(obj.hasAttribute('aria-checked')){
             return obj.getAttribute('aria-checked') === 'true';
         }
+        if('valueAsNumber' in obj){
+            if(obj instanceof HTMLInputElement && obj.type === 'number'){
+                return obj.valueAsNumber;
+            }
+        }
+        if('valueAsDate' in obj){
+            if(obj instanceof HTMLInputElement && obj.type === 'date'){
+                return obj.valueAsDate;
+            }
+        }
         if('value' in obj){
             return obj.value;
         }
