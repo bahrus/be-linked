876802997862aa06c7dc781ef00f9189f8aa9157
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-03-02 08:38:34 -0500

    0.0.73

--- tests/When_Increment.spec.mjs
+++ tests/When_Increment.spec.mjs
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('test3', async ({ page }) => {
+test('When_Increment', async ({ page }) => {
     await page.goto('./tests/When/Increment.html');
     // wait for 2 seconds
     await page.waitForTimeout(2000);
     const editor = page.locator('#target');
     await expect(editor).toHaveAttribute('mark', 'good');
-  });
+});
