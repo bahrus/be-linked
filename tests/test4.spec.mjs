08fa5d292650360acd431ffdd6b20fc3c887f34b
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-03-02 08:05:14 -0500

    0.0.73

--- tests/test4.spec.mjs
+++ tests/test4.spec.mjs
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test4', async ({ page }) => {
     await page.goto('./tests/Link/MathOp.html');
     // wait for 12 seconds
-    await page.waitForTimeout(12000);
+    await page.waitForTimeout(2000);
     const editor = page.locator('#target');
     await expect(editor).toHaveAttribute('mark', 'good');
   });
